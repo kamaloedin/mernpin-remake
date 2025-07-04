3196993dd3ec2d09e9c8c05c21efc4c8d9949118
mkama <kamalimaduddin@gmail.com>
2025-07-04 14:06:07 +0700

    infinite scroll implemented

--- backend/controller/pin.controller.js
+++ backend/controller/pin.controller.js
@@ -1,7 +1,13 @@
 import Pin from '../models/pin.model.js';
 
 export const getPins = async (req, res) => {
-  const pins = await Pin.find();
+  const pageNumber = Number(req.query.cursor) || 0;
+  const LIMIT = 15;
+  const pins = await Pin.find()
+    .limit(LIMIT)
+    .skip(LIMIT * pageNumber);
 
-  res.status(200).json(pins);
+  const hasNextPage = pins.length === LIMIT;
+
+  res.status(200).json({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
 };
